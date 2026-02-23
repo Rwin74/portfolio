b9219b0fff2dab58a917d0f38d980de1e6482db0
Atakan Yağlı <atakan7495@gmail.com>
2026-02-23 22:25:04 +0300

    Güvenlik önlemleri (disable-devtool, seçim/kopyalama engeli) eklendi

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import disableDevtool from 'disable-devtool';
+
+disableDevtool();
 import { BlueprintProvider } from './context/BlueprintContext.jsx'
 
 createRoot(document.getElementById('root')).render(
